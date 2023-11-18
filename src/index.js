949ecf4fd346f7aa17eeb2d59845e00d8bfcef91
123-SHIVA <shivayojha8@gmail.com>
2023-11-19 01:31:09 +0530

    connection established to the mongodb

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv"
+import connectDB from "./db/connectdb.js";
+
+dotenv.config({
+    path: './env'
+})
+connectDB();
+
+
+
+// import express from 'express';
+// const app=express()
+// (async()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         app.on("error",(error)=>{
+//             console.log('Error:',error)
+//             throw error;
+//         })
+
+//         app.listetn(process.env.PORT,()=>{
+//             console.log(`app is listening port ${process.env.PORT}`)
+//         })
+        
+//     } catch (error) {
+//         console.log('ERROR:',error)
+//         throw error;
+        
+//     }
+// })()
